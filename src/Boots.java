38e50a5e3fb9cd66d254c3721415e9b476f2db8b
Piatkowski <irek.piatkowski@nokia.com>
2019-01-30 01:34:18 +0100

    Create class Boots

---
+++ src/Boots.java
@@ -0,0 +1,18 @@
+public class Boots extends Product {
+    private int size;
+    private boolean isNaturalSkin;
+
+    public Boots(int id, String productName, float price, float weight, String color, int productCount, int size, boolean isNaturalSkin) {
+        super(id, productName, price, weight, color, productCount);
+        this.size = size;
+        this.isNaturalSkin = isNaturalSkin;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public boolean isNaturalSkin() {
+        return isNaturalSkin;
+    }
+}
