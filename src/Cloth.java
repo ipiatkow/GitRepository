e2a0b51c3df0a11bc90fdf9ee2302b93f0595610
Piatkowski <irek.piatkowski@nokia.com>
2019-01-30 01:36:41 +0100

    Create class Cloth

---
+++ src/Cloth.java
@@ -0,0 +1,18 @@
+public class Cloth extends Product {
+    private String size;
+    private String material;
+
+    public Cloth(int id, String productName, float price, float weight, String color, int productCount, String size, String material) {
+        super(id, productName, price, weight, color, productCount);
+        this.size = size;
+        this.material = material;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+}
